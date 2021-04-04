9070f9cce9b9714412b8c048bd383c1c2c4fdb0c
xiazhongzheng <1107041574@qq.com>
2021-04-04 22:13:22 +0800

    LRU(最近最少使用)缓存机制

--- suanfa/11.最近最少使用.js
+++ suanfa/11.最近最少使用.js
@@ -23,11 +23,71 @@
 // lRUCache.get(3); // 返回 3
 // lRUCache.get(4); // 返回 4
 
+// var LRUCache = function (capacity = 0) {
+//     //TODO
+//     this.sort = [];
+//     this.cache = {};
+//     this.maxLength = capacity;
+// };
+
+// /** 
+//  * @param {number} key
+//  * @return {number}
+//  */
+// LRUCache.prototype.get = function (key) {
+//     //TODO
+//     let result = this.cache[key];
+//     if (result && result !== undefined) {
+//         this.sort.splice(this.sort.indexOf(key), 1)
+//         this.sort.unshift(key);
+//         console.log('get',result);
+//         return result;
+//     } else {
+//         console.log('get',-1);
+//         return -1;
+//     }
+// };
+
+// /** 
+//  * @param {number} key 
+//  * @param {number} value
+//  * @return {void}
+//  */
+// LRUCache.prototype.put = function (key, value) {
+//     //TODO
+//     if (this.get(key) === -1) {
+//         if (this.sort.length >= this.maxLength) {
+//             let lastKey = this.sort.pop();
+//             delete this.cache[lastKey];
+//         }
+//         this.sort.unshift(key);
+//         this.cache[key] = value;
+//     } else {
+//         this.sort.splice(this.sort.indexOf(key), 1)
+//         this.sort.unshift(key);
+//     }
+//     console.log('cache', this.cache)
+// };
+/**
+ * 
+ * @param {*} key 
+ * @param {*} val 
+ * @param {*} next 
+ * @param {*} pre 
+ */
+var Node = function (key = null, val = null, next = null, pre = null) {
+    //TODO
+    this.key = key;
+    this.val = val;
+    this.next = next;
+    this.pre = pre;
+}
 var LRUCache = function (capacity = 0) {
     //TODO
-    this.sort = [];
-    this.cache = {};
-    this.maxLength = capacity;
+    this.capacity = capacity;
+    this.cache = new Map();
+    this.head = null;
+    this.last = null;
 };
 
 /** 
@@ -36,14 +96,16 @@ var LRUCache = function (capacity = 0) {
  */
 LRUCache.prototype.get = function (key) {
     //TODO
-    let result = this.cache[key];
-    if (result && result !== undefined) {
-        this.sort.splice(this.sort.indexOf(key), 1)
-        this.sort.unshift(key);
-        console.log('get',result);
-        return result;
+    if (this.cache.has(key)) {
+        let node = this.cache.get(key);
+        // 如果不是头部，换成头部
+        if (node !== this.head) {
+            this.toHead(node);
+        }
+        console.log('get', node.val)
+        return node.val;
     } else {
-        console.log('get',-1);
+        console.log('get', -1)
         return -1;
     }
 };
@@ -55,19 +117,62 @@ LRUCache.prototype.get = function (key) {
  */
 LRUCache.prototype.put = function (key, value) {
     //TODO
-    if (this.get(key) === -1) {
-        if (this.sort.length >= this.maxLength) {
-            let lastKey = this.sort.pop();
-            delete this.cache[lastKey];
+    // 如果存在，换成头部，设置新值
+    if (this.cache.has(key)) {
+        let node = this.cache.get(key);
+        node.val = value;
+        // 如果不是头部，换成头部
+        if (node !== this.head) {
+            this.toHead(node);
         }
-        this.sort.unshift(key);
-        this.cache[key] = value;
     } else {
-        this.sort.splice(this.sort.indexOf(key), 1)
-        this.sort.unshift(key);
+        // 如果不存在，判断是否超出，超出了，先删除最后一个
+        if (this.capacity === 0) {
+            let pre = this.last.pre;
+            this.cache.delete(this.last.key);
+            if (pre) {
+                pre.next = null;
+            }
+            this.last = pre;
+        }
+        let oldHead = this.head;
+        let newNode = new Node(key, value, oldHead, null);
+        this.cache.set(key, newNode);
+        if (oldHead) {
+            oldHead.pre = newNode;
+        } else {
+            this.last = newNode;
+        }
+        this.head = newNode;
+        if (this.capacity > 0) {
+            this.capacity--;
+        }
     }
-    console.log('cache', this.cache)
+    console.log('set', this.cache.keys())
 };
+
+LRUCache.prototype.toHead = function (node) {
+    //TODO
+    // 如果是尾部，把前一个换成尾部
+    if (node === this.last) {
+        this.last = node.pre;
+    }
+    // 换成头部，并且前后node相连
+    let preNode = node.pre;
+    let nextNode = node.next;
+    if (preNode) {
+        preNode.next = nextNode;
+    }
+    if (nextNode) {
+        nextNode.pre = preNode;
+    }
+    node.pre = null;
+    node.next = this.head;
+    this.head.pre = node;
+    this.head = node;
+};
+
+
 
 let lRUCache = new LRUCache(2);
 lRUCache.put(1, 1); // 缓存是 {1=1}
