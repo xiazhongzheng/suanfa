e982ccaad8a89591ee8ba3f8194205974cd5d756
xiazhongzheng <1107041574@qq.com>
2021-04-04 21:45:35 +0800

    LRU(最近最少使用)缓存机制

--- suanfa/11.最近最少使用.js
+++ suanfa/11.最近最少使用.js
@@ -55,12 +55,17 @@ LRUCache.prototype.get = function (key) {
  */
 LRUCache.prototype.put = function (key, value) {
     //TODO
-    if (this.sort.length >= this.maxLength) {
-        let lastKey = this.sort.pop();
-        delete this.cache[lastKey];
+    if (this.get(key) === -1) {
+        if (this.sort.length >= this.maxLength) {
+            let lastKey = this.sort.pop();
+            delete this.cache[lastKey];
+        }
+        this.sort.unshift(key);
+        this.cache[key] = value;
+    } else {
+        this.sort.splice(this.sort.indexOf(key), 1)
+        this.sort.unshift(key);
     }
-    this.sort.unshift(key);
-    this.cache[key] = value;
     console.log('cache', this.cache)
 };
 
@@ -69,6 +74,7 @@ lRUCache.put(1, 1); // 缓存是 {1=1}
 lRUCache.put(2, 2); // 缓存是 {1=1, 2=2}
 lRUCache.get(1); // 返回 1
 lRUCache.put(3, 3); // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
+lRUCache.put(1, 1); // 缓存是 {1=1}
 lRUCache.get(2); // 返回 -1 (未找到)
 lRUCache.put(4, 4); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
 lRUCache.get(1); // 返回 -1 (未找到)
